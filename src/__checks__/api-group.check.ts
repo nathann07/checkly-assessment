ee8ac7bcd9667f4b15ce18d5fc810f56b735c0b4
nathann07 <ntmebane@gmail.com>
2024-12-30 18:17:49 -0500

    Remove comments in api-group-check.ts file

--- src/__checks__/api-group.check.ts
+++ src/__checks__/api-group.check.ts
@@ -1,17 +1,6 @@
 import { AlertEscalationBuilder, CheckGroup, RetryStrategyBuilder } from 'checkly/constructs'
 import { emailChannel } from '../alert-channels'
 const alertChannels = [emailChannel]
-/*
-* In this example, we bundle checks using a Check Group. We add checks to this group in two ways:
-* 1. By passing the `CheckGroup` object for the `group` property of the check.
-* 2. By defining a glob pattern like `*.spec.ts` that matches Browser Checks , just like at the Project level, e.g.
-*
-*  browserChecks: {
-*    testMatch: './*.spec.ts'
-*  }
-*
-* You can use either or both. In this example we show option 1.
-**/
 
 export const apiCheckGroup = new CheckGroup('api-check-group', {
   name: 'API Check Group',
